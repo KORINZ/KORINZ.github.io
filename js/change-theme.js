3528c3426e9ab07a2f233976e39f29a93302e6ad
KORINZ <111611023+KORINZ@users.noreply.github.com>
2023-05-16 15:54:55 +0900

    Update theme control

--- js/change-theme.js
+++ js/change-theme.js
@@ -4,36 +4,37 @@ function changeTheme() {
     document.body.classList.toggle('dark');
     button.classList.toggle('dark');
     button.classList.toggle('flipped');
-
+  
     if (document.body.classList.contains('dark')) {
-        localStorage.setItem('darkMode', 'true');
-        localStorage.setItem('flipped', 'true');
-        document.getElementById('theme-icon').classList.remove('fas', 'fa-sun');
-        document.getElementById('theme-icon').classList.add('fas', 'fa-moon');
+      localStorage.setItem('darkMode', 'true');
+      localStorage.setItem('flipped', 'true');
+      document.getElementById('theme-icon').classList.remove('fas', 'fa-sun');
+      document.getElementById('theme-icon').classList.add('fas', 'fa-moon');
     } else {
-        localStorage.setItem('darkMode', 'false');
-        localStorage.setItem('flipped', 'false');
-        document.getElementById('theme-icon').classList.remove('fas', 'fa-moon');
-        document.getElementById('theme-icon').classList.add('fas', 'fa-sun');
+      localStorage.setItem('darkMode', 'false');
+      localStorage.setItem('flipped', 'false');
+      document.getElementById('theme-icon').classList.remove('fas', 'fa-moon');
+      document.getElementById('theme-icon').classList.add('fas', 'fa-sun');
     }
-}
-
-// Event listener for the button
-document.getElementById('theme-toggler').addEventListener('click', changeTheme);
-
-// Check the local storage when the page loads
-window.onload = function () {
+  }
+  
+  // Event listener for the button
+  document.getElementById('theme-toggler').addEventListener('click', changeTheme);
+  
+  // Apply the theme on DOMContentLoaded event
+  document.addEventListener('DOMContentLoaded', function () {
     var button = document.getElementById('theme-toggler');
     if (localStorage.getItem('darkMode') === 'true') {
-        document.body.classList.add('dark');
-        button.classList.add('dark');
-        button.classList.add('flipped');
-        document.getElementById('theme-icon').classList.remove('fas', 'fa-sun');
-        document.getElementById('theme-icon').classList.add('fas', 'fa-moon');
+      document.body.classList.add('dark');
+      button.classList.add('dark');
+      button.classList.add('flipped');
+      document.getElementById('theme-icon').classList.remove('fas', 'fa-sun');
+      document.getElementById('theme-icon').classList.add('fas', 'fa-moon');
     } else {
-        button.classList.remove('dark');
-        button.classList.remove('flipped');
-        document.getElementById('theme-icon').classList.remove('fas', 'fa-moon');
-        document.getElementById('theme-icon').classList.add('fas', 'fa-sun');
+      button.classList.remove('dark');
+      button.classList.remove('flipped');
+      document.getElementById('theme-icon').classList.remove('fas', 'fa-moon');
+      document.getElementById('theme-icon').classList.add('fas', 'fa-sun');
     }
-}
+  });
+  
