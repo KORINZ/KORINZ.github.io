d562af40f7c3e047069a9c52130a10cb96235ff7
KORINZ <111611023+KORINZ@users.noreply.github.com>
2023-05-26 03:50:48 +0900

    Add Tic-Tac-Toe AI project

---
+++ js/tic-tac-toe.js
@@ -0,0 +1,173 @@
+let board = [['', '', ''], ['', '', ''], ['', '', '']];
+let currentPlayer = 'X';
+let maxDepth = 9; // depth for the minimax algorithm
+let gameOver = false;
+
+function drawBoard() {
+    const boardElement = document.getElementById('board');
+    boardElement.innerHTML = '';
+    for (let i = 0; i < 3; i++) {
+        const rowElement = document.createElement('div');
+        rowElement.className = 'row';
+        for (let j = 0; j < 3; j++) {
+            const cellElement = document.createElement('div');
+
+            // Check the board value and create corresponding icon
+            if (board[i][j] === 'X') {
+                const icon = document.createElement('i');
+                icon.className = 'fa-solid fa-x';
+                cellElement.appendChild(icon);
+            } else if (board[i][j] === 'O') {
+                const icon = document.createElement('i');
+                icon.className = 'fa-solid fa-o';
+                cellElement.appendChild(icon);
+            }
+            cellElement.addEventListener('click', () => {
+                if (board[i][j] === '' && currentPlayer === 'X' && !gameOver) { // check for gameOver here
+                    board[i][j] = currentPlayer;
+                    currentPlayer = 'O';
+                    drawBoard();
+                    if (!terminal(board)) {
+                        const { move } = minimax(board, maxDepth, currentPlayer === 'X', -Infinity, Infinity);
+                        if (move) {
+                            board[move[0]][move[1]] = currentPlayer;
+                            currentPlayer = 'X';
+                            drawBoard();
+                        }
+                    }
+                    if (terminal(board)) {
+                        gameOver = true; // set gameOver to true here
+                    }
+                }
+            });
+            rowElement.appendChild(cellElement);
+        }
+        boardElement.appendChild(rowElement);
+    }
+}
+
+function resetBoard() {
+    board = [['', '', ''], ['', '', ''], ['', '', '']];
+    currentPlayer = 'X';
+    const resultElement = document.getElementById('game-result');
+    resultElement.textContent = ''; // clear game result
+    gameOver = false;
+    drawBoard();
+}
+
+function terminal(board) {
+    const lines = [
+        [board[0][0], board[0][1], board[0][2]],
+        [board[1][0], board[1][1], board[1][2]],
+        [board[2][0], board[2][1], board[2][2]],
+        [board[0][0], board[1][0], board[2][0]],
+        [board[0][1], board[1][1], board[2][1]],
+        [board[0][2], board[1][2], board[2][2]],
+        [board[0][0], board[1][1], board[2][2]],
+        [board[0][2], board[1][1], board[2][0]]
+    ];
+    const resultElement = document.getElementById('game-result');
+    if (lines.some(line => line.every(cell => cell === 'X'))) {
+        resultElement.innerHTML = '<strong>You are the winner! Wait, what? That\'s impossible!</strong>';
+        return true;
+    }
+    if (lines.some(line => line.every(cell => cell === 'O'))) {
+        resultElement.innerHTML = '<strong>AI is the winner!</strong>';
+        return true;
+    }
+    if (board.every(row => row.every(cell => cell !== ''))) {
+        resultElement.innerHTML = '<strong>The game is a draw!</strong>';
+        return true;
+    }
+    resultElement.textContent = '';
+    return false;
+}
+
+function winner(board) {
+    const lines = [
+        [board[0][0], board[0][1], board[0][2]],
+        [board[1][0], board[1][1], board[1][2]],
+        [board[2][0], board[2][1], board[2][2]],
+        [board[0][0], board[1][0], board[2][0]],
+        [board[0][1], board[1][1], board[2][1]],
+        [board[0][2], board[1][2], board[2][2]],
+        [board[0][0], board[1][1], board[2][2]],
+        [board[0][2], board[1][1], board[2][0]]
+    ];
+    if (lines.some(line => line.every(cell => cell === 'X'))) {
+        return 'X';
+    }
+    if (lines.some(line => line.every(cell => cell === 'O'))) {
+        return 'O';
+    }
+    return null;
+}
+
+function actions(board) {
+    let moves = [];
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] === '') {
+                moves.push([i, j]);
+            }
+        }
+    }
+    return moves;
+}
+
+function result(board, action, player) {
+    let newBoard = JSON.parse(JSON.stringify(board));
+    newBoard[action[0]][action[1]] = player;
+    return newBoard;
+}
+
+function utility(board) {
+    let win = winner(board);
+    if (win === 'X') return 1;
+    else if (win === 'O') return -1;
+    else return 0;
+}
+
+function minimax(board, depth, isMaximizingPlayer, alpha, beta) {
+    if (terminal(board) || depth === 0) {
+        return { score: utility(board) };
+    }
+
+    let bestMove = null;
+
+    if (isMaximizingPlayer) {
+        let maxEval = { score: -Infinity };
+        let possibleActions = actions(board);
+
+        for (let i = 0; i < possibleActions.length; i++) {
+            let eval = minimax(result(board, possibleActions[i], isMaximizingPlayer ? 'X' : 'O'), depth - 1, !isMaximizingPlayer, alpha, beta);
+            if (eval.score > maxEval.score) {
+                maxEval = { score: eval.score, move: possibleActions[i] };
+                bestMove = possibleActions[i];
+            }
+            alpha = Math.max(alpha, eval.score);
+            if (beta <= alpha) {
+                break;
+            }
+        }
+        return maxEval;
+    } else {
+        let minEval = { score: Infinity };
+        let possibleActions = actions(board);
+
+        for (let i = 0; i < possibleActions.length; i++) {
+            let eval = minimax(result(board, possibleActions[i], isMaximizingPlayer ? 'X' : 'O'), depth - 1, !isMaximizingPlayer, alpha, beta);
+            if (eval.score < minEval.score) {
+                minEval = { score: eval.score, move: possibleActions[i] };
+                bestMove = possibleActions[i];
+            }
+            beta = Math.min(beta, eval.score);
+            if (beta <= alpha) {
+                break;
+            }
+        }
+        return minEval;
+    }
+}
+
+drawBoard();
