4c243e53e62e8aa00eb06fd66a6db28171457f88
KORINZ <111611023+KORINZ@users.noreply.github.com>
2026-06-01 22:37:36 +0900

    Refactor sidebar toggle event listeners to use native JavaScript and enhance sidebar toggler styling

--- js/sidebar-toggle.js
+++ js/sidebar-toggle.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const sidebarToggler = document.getElementById('sidebar-toggler');
     const sidebarIcon = document.getElementById('sidebar-icon');
     const nav = document.getElementById('nav');
 
-    $(nav).on('shown.bs.collapse', function () {
+    nav.addEventListener('shown.bs.collapse', function () {
         sidebarIcon.classList.remove('fa-bars');
         sidebarIcon.classList.add('fa-times');
     });
 
-    $(nav).on('hidden.bs.collapse', function () {
+    nav.addEventListener('hidden.bs.collapse', function () {
         sidebarIcon.classList.remove('fa-times');
         sidebarIcon.classList.add('fa-bars');
     });
