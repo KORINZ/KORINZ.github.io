44cedda0bf17a815f9417dbf48fd8b9ecd071687
KORINZ <111611023+KORINZ@users.noreply.github.com>
2026-06-04 00:16:36 +0900

    Remove button blur effect from theme change function for improved UX

--- js/change-theme.js
+++ js/change-theme.js
@@ -18,7 +18,6 @@ function changeTheme() {
   button.classList.toggle('flipped');
 
   if (typeof drawAnalogClock === 'function') drawAnalogClock(new Date());
-  button.blur();
 }
 
 // Event listener for the button
